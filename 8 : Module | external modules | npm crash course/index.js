1c3bcaafe5e0d2fa48510be9449cfa65547d41a7
git-anik-3 <anik.devxhub@gmail.com>
2023-05-07 23:32:35 +0600

    8 : Module | external modules | npm crash course

---
+++ 8 : Module | external modules | npm crash course/index.js
@@ -0,0 +1,6 @@
+console.log("8 : Module | external modules | npm crash course");
+const randomFruitsName = require("random-fruits-name");
+console.log(randomFruitsName("en"));
+
+const movieNames = require("movies-names");
+console.log(movieNames.random());
